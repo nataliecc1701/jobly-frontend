d4d9a7183e7e08245ce68c609d2d8821d57307e7
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-19 16:20:14 -0700

    jobs are now searchable

--- frontend/src/JobsList.jsx
+++ frontend/src/JobsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import JoblyApi from "./api";
 import Loading from "./Loading";
 import JobCard from "./JobCard";
+import JobsSearchForm from "./JobsSearchForm";
 
 const JobsList = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -18,9 +19,23 @@ const JobsList = () => {
         listJobs()
     }, [query])
     
+    function search(formData) {
+        const q = {}
+        for (const key of Object.keys(formData)) {
+            if (formData[key]) {
+                if (key !== "nameLike") q[key] = formData[key];
+                else q.name = formData.nameLike;
+            }
+        }
+        
+        setIsLoading(true);
+        setQuery(q);
+    }
+    
     if (isLoading) return <Loading />
     
     return <>
+        <JobsSearchForm search={search} query={query} />
         <h2>Displaying {jobs.length} jobs</h2>
         {jobs.map((job) => <JobCard job={job} key={job.id} />)}
     </>
