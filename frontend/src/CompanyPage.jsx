cf4832bb10f187ca0e11eb059d285db395b0d412
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-19 02:59:58 -0700

    companypage now displays company details

--- frontend/src/CompanyPage.jsx
+++ frontend/src/CompanyPage.jsx
@@ -1,10 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import JoblyApi from "./api";
+import Loading from "./Loading";
 
 const CompanyPage = () => {
     const { id } = useParams();
+    let [isLoading, setIsLoading] = useState(true);
+    let [company, setCompany] = useState();
+    let [errMsg, setErrMsg] = useState(null)
     
-    return <div>Placeholder company details for {id}</div>
+    useEffect(() => {
+        async function getCompany(id) {
+            const res = await JoblyApi.getCompany(id)
+            if (res.success) {
+                setCompany(res.company);
+                setErrMsg(null)
+            }
+            else {
+                setErrMsg(`Error code ${res.status}: ${res.message}`)
+            }
+            setIsLoading(false);
+        }
+        
+        getCompany(id);
+    }, [])
+    
+    if (isLoading) return <Loading />
+    
+    if (errMsg) return <div>{errMsg}</div>
+    
+    return <>
+        <h2>{company.name}</h2>
+        <div>{company.description}</div>
+    </>
 }
 
 export default CompanyPage;
