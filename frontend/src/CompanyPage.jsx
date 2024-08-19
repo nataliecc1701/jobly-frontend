9a0534e7358e6f3bc6a7ac13bf00ad3e4509080d
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-18 23:25:24 -0700

    fixed useParams call

--- frontend/src/CompanyPage.jsx
+++ frontend/src/CompanyPage.jsx
@@ -1,7 +1,8 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 const CompanyPage = () => {
-    const { id } = useParams;
+    const { id } = useParams();
     
     return <div>Placeholder company details for {id}</div>
 }
