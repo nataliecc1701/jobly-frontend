85b5dec7b070a3d861f9842d2c3ebf1ec16d20b4
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-19 16:20:48 -0700

    added login page mockup

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
 const LoginPage = () => {
-    return <div>Placeholder for a login form</div>
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    })
+    
+    const handleChange = (evt) => {
+        const { name, value } = evt.target;
+        setFormData({...formData, [name]: value});
+    }
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        login(formData);
+    }
+    
+    return <form className="LoginPage" onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" value={formData.username} onChange={handleChange} /><br/>
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange} /><br/>
+        <input type="submit" value="Log In" /><br/>
+        <Link to="/signup">New user? Create an account</Link>
+    </form>
 }
 
 export default LoginPage;
